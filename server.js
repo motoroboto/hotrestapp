316705efdf4c9d1196fb8ef295f30b18a9a2e215
motoroboto <robert.a.cullen@gmail.com>
2020-11-10 20:53:19 -0500

    posting

--- server.js
+++ server.js
@@ -11,29 +11,43 @@ var waitlist = [];
 
 // Routes
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/tables", function (req, res) {
-  res.sendFile(path.join(__dirname, "tables-page.html"));
+    res.sendFile(path.join(__dirname, "tables-page.html"));
 });
 
 app.get("/reserve", function (req, res) {
-  res.sendFile(path.join(__dirname, "reservation-form.html"));
+    res.sendFile(path.join(__dirname, "reservation-form.html"));
 });
 
 app.get("/api/tables", function (req, res) {
-  console.log("body:", req.body);
-  return res.json(tables);
+    console.log("body:", req.body);
+    return res.json(tables);
 });
 
+
 app.post("/api/tables", (req, res) => {
-  let newTable = req.body;
-  tables.push(newTable);
-  res.json(newTable);
-  console.log("body:", tables);
+    let newTable = req.body;
+    tables.push(newTable);
+    res.json(newTable);
+    console.log("body:", tables);
 });
 
 app.listen(PORT, function () {
-  console.log("app listening on PORT" + PORT);
+    console.log("app listening on PORT" + PORT);
 });
+
+
+// $(".submit").on(click, function () {
+//     var newReservation = {
+//         name: $("#reserve_name").val().trim(),
+//         phone: $("#reserve_phone").val().trim(),
+//         email: $("#reserve_email").val().trim(),
+//         id: $("#reserve_uniqueID").val().trim(),
+//     }
+//         .post("/api/tables", newReservation);
+//     // console.log('newReservation:', newReservation)
+//     console.log('tables', tables)
+// });
