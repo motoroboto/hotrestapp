affad3de5a49bf8ec7d536567da9244fabfccd23
motoroboto <robert.a.cullen@gmail.com>
2020-11-10 19:41:09 -0500

    added more code

--- server.js
+++ server.js
@@ -5,10 +5,37 @@ var app = express();
 var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+const tables = require("./tables");
 var reservedTables = [];
 var waitlist = [];
 
 // Routes 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables-page.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reservation-form.html"));
+});
+
+app.get('/api/tables', function (req, res) {
+    console.log('body:', req.body)
+    return res.json(tables);
+});
+
+app.post('/api/tables'), function (req, res) {
+    var newTable = req.body;
+    newTable.customerName = newTable.name
+    newTable.phoneNumber = newTable.phoneNumber
+    newTable.customerEmail = newTable.email
+    newTable.customerId = newtable.id
+    res.json(newTable);
+};
+
+app.listen(PORT, function () {
+    console.log('app listening on PORT' + PORT);
 });
